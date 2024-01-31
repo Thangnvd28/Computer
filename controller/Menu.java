7dd1ce638353e303732b7748bc328c3d6fc86b57
Thangnvd28 <bithang28@gmail.com>
2024-01-31 08:15:05 +0700

    Computer project

---
+++ controller/Menu.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import util.Validation;
+
+/**
+ *
+ * @author Admin
+ */
+public class Menu {
+    public static int chooseInputOption(){
+        System.out.println("========= Calculator Program =========");
+        System.out.println("1. Normal Calculator");
+        System.out.println("2. BMI Calculator");
+        System.out.println("3. Exit");
+        System.out.println();
+        System.out.println("Please choice one option: ");
+        int option = Validation.inputOption(1, 3);
+        return option;
+    }
+}
